c97d5356d1bc96fc5afe6e061ac6151b7dd4815f
Alwaz <alwazkazi3@gmail.com>
2024-09-11 21:23:56 +0500

    feat(header): added banner

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,18 @@
-export const Navbar = () => {
-  return null;
+import ArrowRight from "@/assets/arrow-right.svg";
+
+export const Header = () => {
+  return (
+    <header>
+      {/* banner */}
+      <div className=" bg-black text-white text-sm flex justify-center items-center py-3">
+        <div className="flex gap-1  items-center">
+          <p>Get started for free</p>
+          <ArrowRight className="w-4 h-4 inline-flex justify-center items-center" />
+        </div>
+      </div>
+
+      {/* Navbar */}
+      <div>navbar</div>
+    </header>
+  );
 };
